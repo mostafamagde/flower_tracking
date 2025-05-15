cf8fd7443c3f3efde398655c366e82d5e16a9524
mostafamagde <mostafamagde227@gmail.com>
2025-05-15 14:19:18 +0300

    merge

--- lib/core/routes_manager/routes_names.dart
+++ lib/core/routes_manager/routes_names.dart
@@ -1,6 +1,6 @@
 class RoutesNames {
-  static const String layout = "/";
-  static const String onboarding = "/onboarding";
+
+  static const String onboarding = "/";
   static const String layout = "layout";
   static const String login = "login";
   static const String home = "home";
